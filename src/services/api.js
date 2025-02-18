96325f8f57ee831b951ec4622b25818b35713be0
José Vitor Oliveira <108138449+jsevitor@users.noreply.github.com>
2025-02-18 20:45:52 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://projeto-orientado-backend.onrender.com/",
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://projeto-orientado-backend.onrender.com/",
+  // baseURL: "http://localhost:3001/",
 });
 
 export default api;
